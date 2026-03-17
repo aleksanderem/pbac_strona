f5cfbee9d8bcabb44dea4559742696c4ddf2024a
aleksanderem <amiesak@gmail.com>
2026-03-17 21:29:52 +0100

    Product image larger: 460px height, 600px width

--- components/product-hero.tsx
+++ components/product-hero.tsx
@@ -47,7 +47,7 @@ export default function ProductHero({ imageUrl, imageAlt }: ProductHeroProps) {
         <img
           src={imageUrl}
           alt={imageAlt}
-          className="max-h-[420px] max-w-[500px] object-contain drop-shadow-[0_12px_60px_rgba(179,24,83,0.3)]"
+          className="max-h-[460px] max-w-[600px] object-contain drop-shadow-[0_12px_60px_rgba(179,24,83,0.3)]"
         />
       </div>
     </div>
