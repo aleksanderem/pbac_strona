83d660aa9fae5111c7253e00b32c33d5251aa3d8
aleksanderem <amiesak@gmail.com>
2026-03-18 12:34:11 +0100

    Replace QuoteForm with ContactSection on homepage (single contact form)

--- app/page.tsx
+++ app/page.tsx
@@ -250,7 +250,6 @@ export default function Home() {
       <LocationsSection />
       <TestimonialsSection />
       <LatestBlogSection />
-      <QuoteForm />
       <ContactSection />
       <FaqSection />
       <Footer />
